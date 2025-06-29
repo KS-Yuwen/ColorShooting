8e3783d0b064965d9af2acc018347cbbb8b570c7
KS-YUWEN <mugensakura3@gmail.com>
2025-06-29 14:49:27 +0900

    アイテム用の基底クラス追加。

---
+++ Source/ColorShooting/ItemBase.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemBase.generated.h"
+
+class USphereComponent;
+class UStaticMeshComponent;
+class URotatingMovementComponent;
+
+UCLASS()
+class COLORSHOOTING_API AItemBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AItemBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+	virtual void OnCollected();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<URotatingMovementComponent> RotatingMovementComponent;
+
+};

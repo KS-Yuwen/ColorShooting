107a258e6c2c25bf053825f59f1011d4c86275fe
KS-YUWEN <mugensakura3@gmail.com>
2025-07-13 11:26:42 +0900

    Levelに配置したCameraをBindして使用するように処理を修正。

--- Source/ColorShooting/Private/ColorShootingGameMode.cpp
+++ Source/ColorShooting/Private/ColorShootingGameMode.cpp
@@ -8,6 +8,28 @@ AColorShootingGameMode::AColorShootingGameMode()
 	GameStateClass = AColorShootingGameState::StaticClass();
 }
 
+void AColorShootingGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // Search Level Camera
+    TArray<AActor*> FoundActors;
+    UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName("LevelCamera"), FoundActors);
+    if (FoundActors.Num() > 0)
+    {
+        AActor* LevelCamera = FoundActors[0];
+        if (LevelCamera)
+        {
+            // Set the view target to the level camera
+            APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+            if (PlayerController)
+            {
+                PlayerController->SetViewTargetWithBlend(LevelCamera);
+            }
+        }
+    }
+}
+
 void AColorShootingGameMode::AddScore(const int32 ScoreValue)
 {
     AColorShootingGameState* GS = GetGameState<AColorShootingGameState>();

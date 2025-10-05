f200b3a938998f36173e748d94bf625303ba0004
KS-YUWEN <mugensakura3@gmail.com>
2025-10-05 10:52:12 +0900

    自動追尾型キャラを生成した場合に正しく追尾できていなかった問題の修正。

--- Source/ColorShooting/Private/Character/EnemyChaser.cpp
+++ Source/ColorShooting/Private/Character/EnemyChaser.cpp
@@ -1,10 +1,21 @@
 #include "Character/EnemyChaser.h"
 #include "Kismet/GameplayStatics.h"
 #include "GameFramework/Pawn.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 AEnemyChaser::AEnemyChaser()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Automatically possess this character with an AI controller when spawned
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+
+	// Set up flying movement for the character
+	if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
+	{
+		MoveComp->SetMovementMode(EMovementMode::MOVE_Flying);
+		MoveComp->GravityScale = 0.0f;
+	}
 }
 
 void AEnemyChaser::BeginPlay()
@@ -14,6 +25,12 @@ void AEnemyChaser::BeginPlay()
 	// Get a reference to the player pawn
 	PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
 
+	// Set the movement speed from the ChaseSpeed property
+	if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
+	{
+		MoveComp->MaxWalkSpeed = ChaseSpeed;
+	}
+
 	// Stop the default fire timer from the parent class
 	GetWorldTimerManager().ClearTimer(M_FireTimerHandle);
 }
@@ -22,9 +39,14 @@ void AEnemyChaser::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// Re-acquire the player pawn if the reference is lost (e.g., after player respawns)
 	if (!PlayerPawn.IsValid())
 	{
-		return;
+		PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+		if (!PlayerPawn.IsValid())
+		{
+			return; // Stop processing if player is not available
+		}
 	}
 
 	// --- Rotation ---
@@ -33,7 +55,10 @@ void AEnemyChaser::Tick(float DeltaTime)
 	SetActorRotation(FRotator(0.f, NewRotation.Yaw, 0.f)); // We only want to rotate in the Yaw
 
 	// --- Movement ---
-	AddMovementInput(GetActorForwardVector());
+	if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
+	{
+		MoveComp->Velocity = GetActorForwardVector() * ChaseSpeed;
+	}
 
 	// --- Combat ---
 	const float DistanceToPlayer = FVector::Dist(GetActorLocation(), PlayerPawn->GetActorLocation());
